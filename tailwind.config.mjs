c5a89bca43d2e25237667c5fa816978972d28331
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2024-03-05 23:22:26 +0200

    adding packages and start building the project with mobile first method

---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+  prefix: "",
+  theme: {
+    fontFamily: {
+      Sans: ["Open Sans", "sans-serif"],
+      Raleway: ["Raleway", "sans-serif"],
+    },
+    screens: {
+      'sm': "375px",
+      'md': "768px",
+      'lg': "1024px",
+      'xl': '1280px',
+      '2xl': '1442px',
+    },
+    extend: {
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
